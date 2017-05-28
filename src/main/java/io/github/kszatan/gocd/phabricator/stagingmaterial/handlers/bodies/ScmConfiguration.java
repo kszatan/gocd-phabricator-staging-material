4d02c20bce5cdfec8f5b78e54070fc95639366cd
Krzysztof Szatan <kszatan@gmail.com>
2017-05-28 14:33:20 +0200

    Add ScmConfiguration

---
+++ src/main/java/io/github/kszatan/gocd/phabricator/stagingmaterial/handlers/bodies/ScmConfiguration.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2017 kszatan
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package io.github.kszatan.gocd.phabricator.stagingmaterial.handlers.bodies;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+public class ScmConfiguration {
+    public String url;
+    public String username;
+    public String password;
+
+    public ScmConfiguration() {
+        url = "";
+        username = "";
+        password = "";
+    }
+
+    public ScmConfiguration(String json) throws InvalidScmConfigurationStringException {
+        JsonParser parser = new JsonParser();
+        JsonObject root = parser.parse(json).getAsJsonObject();
+        if (!root.has("scm-configuration")) {
+            throw new InvalidScmConfigurationStringException();
+        }
+        JsonObject configuration = root.get("scm-configuration").getAsJsonObject();
+
+        url = getField(configuration, "url");
+        username = getField(configuration, "username");
+        password = getField(configuration, "password");
+    }
+    
+    public boolean hasCredentials() {
+        return !username.isEmpty() && !password.isEmpty();
+    }
+
+    private String getField(JsonObject jsonObject, String field) throws InvalidScmConfigurationStringException {
+        if (!jsonObject.has(field)) {
+            throw new InvalidScmConfigurationStringException();
+        }
+        return jsonObject.get(field).getAsJsonObject().get("value").getAsString();
+    }
+}
---
+++ src/test/java/io/github/kszatan/gocd/phabricator/stagingmaterial/handlers/bodies/ScmConfigurationTest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2017 kszatan
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package io.github.kszatan.gocd.phabricator.stagingmaterial.handlers.bodies;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.*;
+
+public class ScmConfigurationTest {
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+    
+    @Test
+    public void constructorShouldParseCorrectJsonString() throws Exception {
+        String json = "{\"scm-configuration\":{\"url\":{\"value\":\"https://github.com/kszatan/gocd-phabricator-staging-material.git\"},\"username\":{\"value\":\"kszatan\"},\"password\":{\"value\":\"hunter2\"}}}";
+        ScmConfiguration configuration = new ScmConfiguration(json);
+        assertThat(configuration.url, equalTo("https://github.com/kszatan/gocd-phabricator-staging-material.git"));
+        assertThat(configuration.username, equalTo("kszatan"));
+        assertThat(configuration.password, equalTo("hunter2"));
+    }
+
+    @Test
+    public void constructorShouldThrowAnExceptionGivenInvalidJson() throws Exception {
+        thrown.expect(InvalidScmConfigurationStringException.class);
+        String json = "{\"scm-view\":2}";
+        new ScmConfiguration(json);
+    }
+}
